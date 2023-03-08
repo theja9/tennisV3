5004174c83578891082d4988dd0e225b5cce225f
Thejashree <thejashreehk@gmail.com>
2023-03-08 15:02:07 +0530

    feat: enable returning tennis score

--- src/main/java/com/example/tennisV3/controller/TennisController.java
+++ src/main/java/com/example/tennisV3/controller/TennisController.java
@@ -2,6 +2,7 @@ package com.example.tennisV3.controller;
 
 import com.example.tennisV3.enums.Scorer;
 import com.example.tennisV3.service.PlayerService;
+import com.example.tennisV3.service.ScoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,10 @@ public class TennisController {
     @Autowired
     private PlayerService players;
 
+    @Autowired
+    private ScoreService scoreService;
+
+
     @GetMapping("/updateScore")
     public void updateScore(@RequestParam Scorer scorer) {
         if (scorer.equals(Scorer.PLAYER_ONE)) {
@@ -22,5 +27,10 @@ public class TennisController {
         } else if (scorer.equals(Scorer.PLAYER_TWO)) {
             players.playerTwoScores();
         }
+    }
+
+    @GetMapping("/getScore")
+    public String getScore() {
+        return scoreService.getScore(players.getPlayerOnePoints(), players.getPlayerTwoPoints());
     }
 }
--- src/test/java/com/example/tennisV3/controller/TennisControllerTest.java
+++ src/test/java/com/example/tennisV3/controller/TennisControllerTest.java
@@ -2,6 +2,8 @@ package com.example.tennisV3.controller;
 
 import com.example.tennisV3.enums.Scorer;
 import com.example.tennisV3.service.PlayerService;
+import com.example.tennisV3.service.ScoreService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.mockito.Mockito;
@@ -14,6 +16,8 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(TennisController.class)
 class TennisControllerTest {
@@ -24,13 +28,16 @@ class TennisControllerTest {
     @MockBean
     private PlayerService players;
 
+    @MockBean
+    private ScoreService scoreService;
+
     @ParameterizedTest
     @EnumSource(Scorer.class)
     void updateScoreTest(Scorer scorer) throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/tennis/updateScore")
                 .param("scorer", scorer.name())
                 .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(status().isOk());
 
         if (scorer.equals(Scorer.PLAYER_ONE)) {
             verify(players, Mockito.times(1)).playerOneScores();
@@ -38,4 +45,16 @@ class TennisControllerTest {
             verify(players, Mockito.times(1)).playerTwoScores();
         }
     }
+
+    @Test
+    public void testGetScore() throws Exception {
+        String expectedScore = "Love All";
+        when(scoreService.getScore(Mockito.anyInt(), Mockito.anyInt()))
+                .thenReturn(expectedScore);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/tennis/getScore"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(expectedScore));
+    }
+
 }
